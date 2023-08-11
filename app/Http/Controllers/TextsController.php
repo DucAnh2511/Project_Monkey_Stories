ce7fdc24d1d8aae507e58b6dfa05123bbda9dabf
DucAnh2511 <ducanh25112002tb@gmail.com>
2023-08-11 19:13:40 +0700

    fix error link at route

--- app/Http/Controllers/TextsController.php
+++ app/Http/Controllers/TextsController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\TextRepository;
 use Illuminate\Http\Request;
 
 class TextsController extends Controller
 {
     //
+    protected $classRepository;
+    public function __construct(TextRepository $classRepository)
+    {
+        $this->classRepository = $classRepository;
+    }
+    public function index(){
+        return $this->classRepository->getAll();
+    }
+    public function create(Request $request){
+        return $this->classRepository->create($request->all());
+    }
+    public function show($id){
+        return $this->classRepository->show($id);
+    }
+    public function update(Request $request,$id){
+        return $this->classRepository->update($id,$request->all());
+    }
+    public  function delete($id){
+        return $this->classRepository->delete($id);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +11,9 @@ use App\Http\Controllers\ProductController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StoriesController;
+use App\Http\Controllers\PagesController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -23,6 +24,13 @@ Route::prefix('api')->group(function (){
         Route::post('/',[StoriesController::class,'create'])->name('story.create');
         Route::put('/{id}',[StoriesController::class,'update'])->name('story.update');
         Route::delete('/{id}',[StoriesController::class,'delete'])->name('story.delete');
+    });
+    Route::prefix('page')->group(function (){
+        Route::get('/',[PagesController::class,'index'])->name('page.index');
+        Route::get('/{id}',[PagesController::class,'show'])->name('page.show');
+        Route::post('/',[PagesController::class,'create'])->name('page.create');
+        Route::put('/{id}',[PagesController::class,'update'])->name('page.update');
+        Route::delete('/{id}',[PagesController::class,'delete'])->name('page.delete');
     });
 
 });
